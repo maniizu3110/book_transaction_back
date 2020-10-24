d42aec7787f37f069347cb7b323b5877712ad1ca
maniizu3110 <maniizu3110@gmail>
2020-10-24 22:10:46 +0900

    modify for production use

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 
 if Rails.env == "production"
-    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "jdh-authentication-app-api.herokuapp.com"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "book-transaction-app.web.app"
   else
     Rails.application.config.session_store :cookie_store, key: "_authentication_app"
   end
