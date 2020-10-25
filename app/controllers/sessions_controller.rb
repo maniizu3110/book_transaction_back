71f61cc6e2b99da32d21fada72f872b045c47f40
maniizu3110 <maniizu3110@gmail>
2020-10-26 08:27:00 +0900

    remodify registration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User
             .find_by(email: params["user"]["email"])
-            # .try(:authenticate, params["user"]["password"])
+            .try(:authenticate, params["user"]["password"])
 
     if user
       session[:user_id] = user.id
