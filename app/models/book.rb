c51669c9e8433c9fe2e9700953c28bb91381f3ff
maniizu3110 <maniizu3110@gmail>
2020-10-26 21:05:12 +0900

    delete price validation

--- app/models/book.rb
+++ app/models/book.rb
@@ -5,7 +5,6 @@ class Book < ApplicationRecord
   validates :title,presence: true
   validates :price,presence: true
   validates :major,presence: true
-  validates :price,presence: true
   validates :condition,presence: true
   validates :user_id,presence: true
   validates :content, presence: true, length: { maximum: 140 }
